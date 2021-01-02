a25ffc16386d386a0d54fd51d923b931fc42048b
Tatyana Gonzaga <tmugonzaga@gmail.com>
2021-01-02 20:48:32 +0800

    js generated lists

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
+// PROGRAMS / COURSES GENERATION
+
 const progsOnHome = () => {
-    // console.log('hi');
     let html = '';
     for (let index = 0; index < 4; index++) {
         html += `
@@ -28,3 +29,69 @@ const fullPrograms = () => {
     });
     document.getElementById('fullPrograms').innerHTML = html;
 }
+
+const coursePage = (course) => {
+    var courseInfo = programs[course];
+    let html1 = `
+        <div class="m-3 card" style="max-width: 100%;">
+            <img src="${courseInfo.image}" class="card-img" alt="">
+        </div>
+        <div class="m-3 card" style="max-width: 100%;">
+            <ul class="list-group list-group-flush">
+                <li class="list-group-item">${courseInfo.instructor}</li>
+            </ul>
+            <div class="card-footer">
+                <h6 class="mt-2 card-title text-center">Course Modules:</h6>
+                <p id="moduleList" class="px-2 card-text">
+                    ${fullModuleList(course)}
+                </p>
+            </div>
+        </div>`;
+    document.getElementById('courseImgList').innerHTML = html1;
+
+    let html2 = `
+        <div class="m-3 card" style="min-width: 100%;">
+            <div class="card-body">
+                <h2 class="m-3 card-title">${courseInfo.title}</h2>
+                <p class="mt-3 p-3 card-text">
+                ${courseInfo.description}
+                </p>
+            </div>
+        </div>`;
+    document.getElementById('courseDescription').innerHTML = html2;
+}
+
+const fullModuleList = (course) => {
+    let html = '';
+    programs[course].modules.forEach(index => {
+        html += `&#9634;&nbsp;&nbsp;&nbsp;${index}<br>`;
+        console.log(html);
+    });
+    return html;
+}
+
+
+// TEAM GENERATION
+
+const fullTeam = () => {
+    let html = '';
+    team.forEach(index => {
+        html += `
+        <div class="m-2 d-flex flex-column">
+            <img src="${index.image}" alt="">
+            <p class="mt-2 name">${index.name}</p>
+            <p class="description">${index.role}</p>
+        </div>`
+    });
+    document.getElementById('teamList').innerHTML = html;
+}
+
+// const showInfo = (index) => {
+//     let html = 
+//     `<div class="m-3 card" style="max-width: 10rem; position: absolute; z-index: 10;">
+//         <div class="card-body">
+//             <h2 class="m-3 card-title">${index.name}</h2>
+//         </div>
+//     </div>`;
+//     return html;
+// }
---
+++ team.js
@@ -0,0 +1,90 @@
+var team = [
+    {
+        'name': 'Ysabelle David',
+        'role': 'CEO & COO',
+        'image': 'assets/img/VSr Team Photos/Ysabelle David.jpg',
+        'socials': [
+            {'fb': ''},
+            {'ig': ''},
+            {'in': ''},
+        ],
+        'description': `<span style="text-decoration: line-through;">Hello</span>`,
+    },
+    {
+        'name': 'Junny Cerdeña',
+        'role': 'President',
+        'image': 'assets/img/VSr Team Photos/Junny Cerdeña.jpg',
+        'socials': [
+            {'fb': ''},
+            {'ig': ''},
+            {'in': ''},
+        ],
+        'description': `Former Food Technologist who decided to "Make the Switch" back in 2018. Since then, Junny has been a Virtual Assistant specializing in Administrative and Technical Roles. Tech whiz🤓 + Organizing freak. `,
+    },
+    {
+        'name': 'Jan Rey Cerdeña',
+        'role': 'Graphic Designer',
+        'image': 'assets/img/VSr Team Photos/Jan Rey Cerdeña.jpg',
+        'socials': [
+            {'fb': ''},
+            {'ig': ''},
+            {'in': ''},
+        ],
+        'description': ``,
+    },
+    {
+        'name': 'Tatyana Gonzaga',
+        'role': 'Web Developer',
+        'image': 'assets/img/VSr Team Photos/Tatyana Gonzaga.JPG',
+        'socials': [
+            {'fb': ''},
+            {'ig': ''},
+            {'in': ''},
+        ],
+        'description': `I code for a living, but mostly for marketing. I brew coffee at home like a professional. I care for plants like they are my babies.`,
+    },
+    {
+        'name': 'Maki Kishore',
+        'role': 'Customer Support',
+        'image': 'assets/img/VSr Team Photos/Maki Kishore.jpg',
+        'socials': [
+            {'fb': ''},
+            {'ig': ''},
+            {'in': ''},
+        ],
+        'description': ``,
+    },
+    {
+        'name': 'Stef Arellano-Rañosa',
+        'role': 'Ads Specialist',
+        'image': 'assets/img/VSr Team Photos/Stef Arellano-Rañosa.jpg',
+        'socials': [
+            {'fb': ''},
+            {'ig': ''},
+            {'in': ''},
+        ],
+        'description': ``,
+    },
+    {
+        'name': 'Ja Casanova-Garrido',
+        'role': 'SEO Specialist',
+        'image': 'assets/img/VSr Team Photos/Ja Casanova-Garrido.jpg',
+        'socials': [
+            {'fb': ''},
+            {'ig': ''},
+            {'in': ''},
+        ],
+        'description': ``,
+    },
+    {
+        'name': 'RJ Garrido',
+        'role': 'Video Editor',
+        'image': 'assets/img/VSr Team Photos/RJ Garrido.jpg',
+        'socials': [
+            {'fb': ''},
+            {'ig': ''},
+            {'in': ''},
+        ],
+        'description': ``,
+    },
+];
