fa6f40a23cdca23e20db999701f77bc261404706
Tatyana Gonzaga <tmugonzaga@gmail.com>
2021-01-04 22:17:48 +0800

    rough version 1

--- programs.js
+++ programs.js
@@ -1,35 +1,21 @@
 var programs = [
     // VA Basics 0
     {
-        'title':'VA Basics',
+        'title':'Virtual Assistant Jumpstart',
+        'subtitle':'',
         'instructor':'Ysabelle David',
         'image':'assets/img/Programs/VABasics.png',
-        'page':'Programs_VABasics.html',
-        'modules': ["Module 1: What are VA's?", 
-            "Module 2: VA Services",
-            "Module 3: Tools",
-            "Module 4: Rates",
-            "Module 5: Hardware",
-            "Module 6: Branding",
-            "Module 7: Legalities",
-            "Module 8: Roadmap",
-            "Bonus Content"],
-        'description': `Tired of your 9-5 job? Is the exhaustion of commuting of driving to work already stressing you out? Do you want, for once, to be able to get-togethers with your friends without them having to worry about your work schedule? Is your dream to be able to spend time with your kids more as they are growing up? 
-        <br><br>
-        If you can relate to these questions, then this course is for you!
-        <br><br>
-        We want you to be able to 'Make the Switch' and be able to do what you want to do, on a schedule that  fits your personal needs! We designed a course that will be able to give you very valuable information about how to start and build your VA life. 
-        <br><br>
-        We built this for the purpose of helping you and make you realize that it is possible to have a work-life balance. That having a job doesn't mean that your personal life / schedule will not have to be compromised. 
-        <br><br>
-        Not only does this course have all the necessary things you need to get started, but will give you a chance to choose the kind of job you will take! `,
+        'page':'',
+        'modules': [],
+        'description': ``,
     },
     // Soft Skills 1
     {
         'title':'Soft Skills',
+        'subtitle':'How to Become a Better Human',
         'instructor':'Ysabelle David',
         'image':'assets/img/Programs/SoftSkills.png',
-        'page':'Programs_SoftSkills.html',
+        'page':'',
         'modules': [
             "Module 1: Communication",
             "Module 2: Motivation, Adaptability, and Stress Management",
@@ -46,27 +32,22 @@ var programs = [
     },
     // Customer Support 2
     {
-        'title':'Customer Support',
+        'title':'Language',
+        'subtitle':'English for Filipinos',
         'instructor':'Ysabelle David',
         'image':'assets/img/Programs/CS.png',
-        'page':'Programs_CustomerSupport.html',
+        'page':'',
         'modules': [],
         'description': ``,
     },
     // Project Management 3
     {
-        'title':'Project Management',
+        'title':'Virtual Assistant Specifics',
+        'subtitle':'',
         'instructor':'Ysabelle David',
         'image':'assets/img/Programs/ProjectMgmt.png',
-        'page':'Programs_ProjectManagement.html',
-        'modules': [
-            "Module 1: Communication",
-            "Module 2: Motivation, Adaptability, and Stress Management",
-            "Module 3: Leadership, Teamwork, and Conflict Resolution",
-            "Module 4: Problem Solving and Critical Thinking",
-            "Module 5: Ethics",
-            "Module 6: Life Management"
-            ],
+        'page':'',
+        'modules': [],
         'description': `
         These days, there are so many online courses that focuses on technical skills, and hard skills. And while those are obviously amazing things to learn and develop, what most of us don't realize is our attitude and personality can be trained to be better too. 
         <br><br>
@@ -84,3 +65,90 @@ var programs = [
     // },
 
 ];
+
+// var programs = [
+//     // VA Basics 0
+//     {
+//         'title':'VA Basics',
+//         'instructor':'Ysabelle David',
+//         'image':'assets/img/Programs/VABasics.png',
+//         'page':'Programs_VABasics.html',
+//         'modules': ["Module 1: What are VA's?", 
+//             "Module 2: VA Services",
+//             "Module 3: Tools",
+//             "Module 4: Rates",
+//             "Module 5: Hardware",
+//             "Module 6: Branding",
+//             "Module 7: Legalities",
+//             "Module 8: Roadmap",
+//             "Bonus Content"],
+//         'description': `Tired of your 9-5 job? Is the exhaustion of commuting of driving to work already stressing you out? Do you want, for once, to be able to get-togethers with your friends without them having to worry about your work schedule? Is your dream to be able to spend time with your kids more as they are growing up? 
+//         <br><br>
+//         If you can relate to these questions, then this course is for you!
+//         <br><br>
+//         We want you to be able to 'Make the Switch' and be able to do what you want to do, on a schedule that  fits your personal needs! We designed a course that will be able to give you very valuable information about how to start and build your VA life. 
+//         <br><br>
+//         We built this for the purpose of helping you and make you realize that it is possible to have a work-life balance. That having a job doesn't mean that your personal life / schedule will not have to be compromised. 
+//         <br><br>
+//         Not only does this course have all the necessary things you need to get started, but will give you a chance to choose the kind of job you will take! `,
+//     },
+//     // Soft Skills 1
+//     {
+//         'title':'Soft Skills',
+//         'instructor':'Ysabelle David',
+//         'image':'assets/img/Programs/SoftSkills.png',
+//         'page':'Programs_SoftSkills.html',
+//         'modules': [
+//             "Module 1: Communication",
+//             "Module 2: Motivation, Adaptability, and Stress Management",
+//             "Module 3: Leadership, Teamwork, and Conflict Resolution",
+//             "Module 4: Problem Solving and Critical Thinking",
+//             "Module 5: Ethics",
+//             "Module 6: Life Management"],
+//         'description': `
+//         These days, there are so many online courses that focuses on technical skills, and hard skills. And while those are obviously amazing things to learn and develop, what most of us don't realize is our attitude and personality can be trained to be better too. 
+//         <br><br>
+//         Most of us are so busy with running our lives and making sure that the bills are paid, do our tasks, or that we meet deadlines that we often forget the very essence of living and becoming a happy and satisfied person. And that's exactly what we will got through in this course. Basically, we will refresh your way of living, the way you handle daily issues with others and most specially, with yourself. 
+//         <br><br>
+//         This course is designed to teach you new (and proven) techniques tricks on how to take care of yourself, and develop new ways to improve your relationships with others. `,
+//     },
+//     // Customer Support 2
+//     {
+//         'title':'Customer Support',
+//         'instructor':'Ysabelle David',
+//         'image':'assets/img/Programs/CS.png',
+//         'page':'Programs_CustomerSupport.html',
+//         'modules': [],
+//         'description': ``,
+//     },
+//     // Project Management 3
+//     {
+//         'title':'Project Management',
+//         'instructor':'Ysabelle David',
+//         'image':'assets/img/Programs/ProjectMgmt.png',
+//         'page':'Programs_ProjectManagement.html',
+//         'modules': [
+//             "Module 1: Communication",
+//             "Module 2: Motivation, Adaptability, and Stress Management",
+//             "Module 3: Leadership, Teamwork, and Conflict Resolution",
+//             "Module 4: Problem Solving and Critical Thinking",
+//             "Module 5: Ethics",
+//             "Module 6: Life Management"
+//             ],
+//         'description': `
+//         These days, there are so many online courses that focuses on technical skills, and hard skills. And while those are obviously amazing things to learn and develop, what most of us don't realize is our attitude and personality can be trained to be better too. 
+//         <br><br>
+//         Most of us are so busy with running our lives and making sure that the bills are paid, do our tasks, or that we meet deadlines that we often forget the very essence of living and becoming a happy and satisfied person. And that's exactly what we will got through in this course. Basically, we will refresh your way of living, the way you handle daily issues with others and most specially, with yourself. 
+//         <br><br>
+//         This course is designed to teach you new (and proven) techniques tricks on how to take care of yourself, and develop new ways to improve your relationships with others. `,
+//     },
+//     // {
+//     //     'title':'XXX',
+//     //     'instructor':'Ysabelle David',
+//     //     'image':'assets/img/Programs/XXX.png',
+//     //     'page':'Programs_XXXXX.html',
+//     //     'modules': [],
+//     //     'description': ``,
+//     // },
+
+// ];
--- script.js
+++ script.js
@@ -4,14 +4,23 @@ const progsOnHome = () => {
     let html = '';
     for (let index = 0; index < 4; index++) {
         html += `
-        <div class="m-4 card" style="max-width: 25rem;" onclick="location.href='${programs[index].page}'">
+        <div class="m-4 card" style="max-width: 25rem;">
             <img src="${programs[index].image}" class="card-img-top" alt="">
-            <div class="card-body">
+            <div class="card-body text-center">
                 <h5 class="card-title">${programs[index].title}</h5>
-                <p class="mt-3 instructor">${programs[index].instructor}</p>
             </div>
         </div>`
     }
+    // for (let index = 0; index < 4; index++) {
+    //     html += `
+    //     <div class="m-4 card" style="max-width: 25rem;" onclick="location.href='${programs[index].page}'">
+    //         <img src="${programs[index].image}" class="card-img-top" alt="">
+    //         <div class="card-body">
+    //             <h5 class="card-title">${programs[index].title}</h5>
+    //             <p class="mt-3 instructor">${programs[index].instructor}</p>
+    //         </div>
+    //     </div>`
+    // }
     document.getElementById('homePrograms').innerHTML = html;
 }
 
@@ -19,14 +28,24 @@ const fullPrograms = () => {
     let html = '';
     programs.forEach(index => {
         html += `
-        <div class="m-3 card" style="max-width: 24rem;" onclick="location.href='${index.page}'">
+        <div class="m-3 card" style="max-width: 25rem;" onclick="location.href='${index.page}'">
             <img src="${index.image}" class="card-img-top" alt="">
-            <div class="card-body">
+            <div class="card-body text-center">
                 <h5 class="card-title">${index.title}</h5>
-                <p class="mt-3 instructor">${index.instructor}</p>
+                <p class="card-text">COMING SOON</p>
             </div>
         </div>`
     });
+    // programs.forEach(index => {
+    //     html += `
+    //     <div class="m-3 card" style="max-width: 24rem;" onclick="location.href='${index.page}'">
+    //         <img src="${index.image}" class="card-img-top" alt="">
+    //         <div class="card-body">
+    //             <h5 class="card-title">${index.title}</h5>
+    //             <p class="mt-3 instructor">${index.instructor}</p>
+    //         </div>
+    //     </div>`
+    // });
     document.getElementById('fullPrograms').innerHTML = html;
 }
 
