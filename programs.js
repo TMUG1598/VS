2ff5f1c188555465d923b39f1b96d62195213792
Tatyana Gonzaga <tmugonzaga@gmail.com>
2021-01-05 21:17:34 +0800

    completion of first version

--- programs.js
+++ programs.js
@@ -1,28 +1,74 @@
 var programs = [
-    // VA Basics 0
     {
         'title':'Virtual Assistant Jumpstart',
-        'subtitle':'',
         'instructor':'Ysabelle David',
         'image':'assets/img/Programs/VABasics.png',
-        'page':'',
-        'modules': [],
-        'description': ``,
+        'page':'Programs_VAJumpstart.html',
+        'modules': [
+            'Module 1: Making the VA Switch',
+            'Module 1.1 - What are VA’s?',
+            'Module 1.2 - What VA’s do?',
+            'Module 1.3 - Why become a VA?',
+            'Module 1.4 - Employment vs. Freelance',
+            'Module 2: VA Services: Discover your Niche',
+            'Module 2.1 - Services you can offer',
+            'Module 2.2 - Determining Which Services Suit You',
+            'Module 2.3 - Your Specializing',
+            'Module 3: Tools',
+            'Module 3.1 - Tools for all VA’s',
+            'Module 3.2 - Tools for your specialty',
+            'Module 4: Rates',
+            'Module 4.1 - The Rate Calculator',
+            'Module 4.2 - Creating Packages',
+            'Module 4.3 - Creating your pricing sheets',
+            'Module 5: Your Gadgets & Contraptions',
+            'Module 5.1 - Equipment & Gears: what you’ll need',
+            'Module 5.2 - Office Set-up',
+            'Module 6: Branding',
+            'Module 6.1 - Business Name',
+            'Module 6.2 - Your Vision statement',
+            'Module 6.3 - Creating your Portfolio',
+            'Module 7: Legalities',
+            'Module 7.1 - Things to register & where',
+            'Module 7.2 - Freelancer rights',
+            'Module 7.3 - Government Mandated Fees: Where to pay & Why you should pay them',
+            'BONUS Content - Basic Contract Template',
+            'Module 8: Getting Clients',
+            'Module 8.1 - Marketing yourself',
+            'Module 8.2 - Where to get clients',
+            'Module 8.3 - How to write a cover letter',
+            'Module 8.4 - Tips on how to ace interviews',
+            'Module 8.5 - Discovery Calls: What are they and how to handle them',
+            'Module 8.6 - Choosing a client & when to turn them down',
+            'Module 8.7 - Onboarding a client]'],
+        'description': `Tired of your 9-5 job? Is the exhaustion of commuting of driving to work already stressing you out? Do you want, for once, to be able to get-togethers with your friends without them having to worry about your work schedule? Is your dream to be able to spend time with your kids more as they are growing up? 
+        <br><br>
+        If you can relate to these questions, then this course is for you!
+        <br><br>
+        We want you to be able to 'Make the Switch' and be able to do what you want to do, on a schedule that  fits your personal needs! We designed a course that will be able to give you very valuable information about how to start and build your VA life. 
+        <br><br>
+        We built this for the purpose of helping you and make you realize that it is possible to have a work-life balance. That having a job doesn't mean that your personal life / schedule will not have to be compromised. 
+        <br><br>
+        Not only does this course have all the necessary things you need to get started, but will give you a chance to choose the kind of job you will take! 
+        `,
     },
-    // Soft Skills 1
     {
-        'title':'Soft Skills',
-        'subtitle':'How to Become a Better Human',
+        'title':'Soft Skills: Become a Better Human',
         'instructor':'Ysabelle David',
         'image':'assets/img/Programs/SoftSkills.png',
-        'page':'',
+        'page':'Programs_SoftSkills.html',
         'modules': [
-            "Module 1: Communication",
-            "Module 2: Motivation, Adaptability, and Stress Management",
-            "Module 3: Leadership, Teamwork, and Conflict Resolution",
-            "Module 4: Problem Solving and Critical Thinking",
-            "Module 5: Ethics",
-            "Module 6: Life Management"],
+            'Module 1: Email Marketing',
+            'Module 2: Customer Service',
+            'Module 3: Social Media Management',
+            'Module 4: Copywriting',
+            'Module 5: FB ads',
+            'Module 6: Lead Generation',
+            'Module 7: Photo & Video Editing',
+            'Module 8: E-commerce',
+            'Module 9: SEO',
+            'Module 10: Project Management',
+            'Module 11: CRM/CMS Building',],
         'description': `
         These days, there are so many online courses that focuses on technical skills, and hard skills. And while those are obviously amazing things to learn and develop, what most of us don't realize is our attitude and personality can be trained to be better too. 
         <br><br>
@@ -30,23 +76,11 @@ var programs = [
         <br><br>
         This course is designed to teach you new (and proven) techniques tricks on how to take care of yourself, and develop new ways to improve your relationships with others. `,
     },
-    // Customer Support 2
-    {
-        'title':'Language',
-        'subtitle':'English for Filipinos',
-        'instructor':'Ysabelle David',
-        'image':'assets/img/Programs/CS.png',
-        'page':'',
-        'modules': [],
-        'description': ``,
-    },
-    // Project Management 3
     {
         'title':'Virtual Assistant Specifics',
-        'subtitle':'',
         'instructor':'Ysabelle David',
-        'image':'assets/img/Programs/ProjectMgmt.png',
-        'page':'',
+        'image':'assets/img/Programs/VASpecifics.png',
+        'page':'error.html',
         'modules': [],
         'description': `
         These days, there are so many online courses that focuses on technical skills, and hard skills. And while those are obviously amazing things to learn and develop, what most of us don't realize is our attitude and personality can be trained to be better too. 
@@ -54,6 +88,14 @@ var programs = [
         Most of us are so busy with running our lives and making sure that the bills are paid, do our tasks, or that we meet deadlines that we often forget the very essence of living and becoming a happy and satisfied person. And that's exactly what we will got through in this course. Basically, we will refresh your way of living, the way you handle daily issues with others and most specially, with yourself. 
         <br><br>
         This course is designed to teach you new (and proven) techniques tricks on how to take care of yourself, and develop new ways to improve your relationships with others. `,
+    },
+    {
+        'title':'Language: English for Filipinos',
+        'instructor':'Ysabelle David',
+        'image':'assets/img/Programs/LanguageforFils.png',
+        'page':'error.html',
+        'modules': [],
+        'description': ``,
     },
     // {
     //     'title':'XXX',
--- script.js
+++ script.js
@@ -4,23 +4,14 @@ const progsOnHome = () => {
     let html = '';
     for (let index = 0; index < 4; index++) {
         html += `
-        <div class="m-4 card" style="max-width: 25rem;">
+        <div class="m-4 card" style="max-width: 25rem;" onclick="location.href='${programs[index].page}'">
             <img src="${programs[index].image}" class="card-img-top" alt="">
             <div class="card-body text-center">
                 <h5 class="card-title">${programs[index].title}</h5>
+                <p class="mt-3 instructor">${programs[index].instructor}</p>
             </div>
         </div>`
-    }
-    // for (let index = 0; index < 4; index++) {
-    //     html += `
-    //     <div class="m-4 card" style="max-width: 25rem;" onclick="location.href='${programs[index].page}'">
-    //         <img src="${programs[index].image}" class="card-img-top" alt="">
-    //         <div class="card-body">
-    //             <h5 class="card-title">${programs[index].title}</h5>
-    //             <p class="mt-3 instructor">${programs[index].instructor}</p>
-    //         </div>
-    //     </div>`
-    // }
+    };
     document.getElementById('homePrograms').innerHTML = html;
 }
 
@@ -29,23 +20,13 @@ const fullPrograms = () => {
     programs.forEach(index => {
         html += `
         <div class="m-3 card" style="max-width: 25rem;" onclick="location.href='${index.page}'">
-            <img src="${index.image}" class="card-img-top" alt="">
             <div class="card-body text-center">
                 <h5 class="card-title">${index.title}</h5>
-                <p class="card-text">COMING SOON</p>
+                <p class="mt-3 instructor text-center">${index.instructor}</p>
             </div>
+            <img src="${index.image}" class="card-img" alt="">
         </div>`
     });
-    // programs.forEach(index => {
-    //     html += `
-    //     <div class="m-3 card" style="max-width: 24rem;" onclick="location.href='${index.page}'">
-    //         <img src="${index.image}" class="card-img-top" alt="">
-    //         <div class="card-body">
-    //             <h5 class="card-title">${index.title}</h5>
-    //             <p class="mt-3 instructor">${index.instructor}</p>
-    //         </div>
-    //     </div>`
-    // });
     document.getElementById('fullPrograms').innerHTML = html;
 }
 
@@ -53,14 +34,10 @@ const coursePage = (course) => {
     var courseInfo = programs[course];
     let html1 = `
         <div class="m-3 card" style="max-width: 100%;">
-            <img src="${courseInfo.image}" class="card-img" alt="">
-        </div>
-        <div class="m-3 card" style="max-width: 100%;">
             <ul class="list-group list-group-flush">
-                <li class="list-group-item">${courseInfo.instructor}</li>
+                <li class="list-group-item">Course Modules:</li>
             </ul>
             <div class="card-footer">
-                <h6 class="mt-2 card-title text-center">Course Modules:</h6>
                 <p id="moduleList" class="px-2 card-text">
                     ${fullModuleList(course)}
                 </p>
@@ -69,10 +46,13 @@ const coursePage = (course) => {
     document.getElementById('courseImgList').innerHTML = html1;
 
     let html2 = `
+        <div class="m-3 card" style="max-width: 100%; position: relative; z-index:1; top: 70px;">
+            <img src="${courseInfo.image}" class="card-img" alt="">
+        </div>
         <div class="m-3 card" style="min-width: 100%;">
             <div class="card-body">
-                <h2 class="m-3 card-title">${courseInfo.title}</h2>
-                <p class="mt-3 p-3 card-text">
+                <h2 class="mt-5 mx-3 card-title">${courseInfo.title}</h2>
+                <p class="mt-3 p-4 card-text">
                 ${courseInfo.description}
                 </p>
             </div>
@@ -92,15 +72,64 @@ const fullModuleList = (course) => {
 
 // TEAM GENERATION
 
+const instructorsList = () => {
+    let html = '';
+    team.forEach(index => {
+        if (index.teacher == 'True') {
+            html += `
+            <div class="m-2 d-flex flex-column">
+                <img src="${index.image}" alt="">
+                <p class="mt-2 name">${index.name}</p>
+                <p class="role">${index.role}</p>
+            </div>`
+        }
+        
+    });
+    document.getElementById('instructorList').innerHTML = html;
+}
+
 const fullTeam = () => {
     let html = '';
     team.forEach(index => {
         html += `
-        <div class="m-2 d-flex flex-column">
-            <img src="${index.image}" alt="">
-            <p class="mt-2 name">${index.name}</p>
-            <p class="description">${index.role}</p>
-        </div>`
+        <div class="d-flex flex-column align-items-center text-center">
+            <div class="d-flex justify-content-center" style="position: relative; z-index:1; top: 100px;">
+                <img src="${index.image}" alt="" style="max-width: 200px">
+            </div>
+            <div class="mx-3 px-3 card" style="max-width: 350px; padding-top: 100px;">
+                <div class="card-body d-flex flex-column justify-content-start align-items-center text-center">
+                    <p class="name">${index.name}</p>
+                    <p class="role">${index.role}</p><br/>
+                    <p class="description">${index.description}</p><br>
+                    <div class="w-100 p-3 socials" style="border-top: 2px solid #01132B;">
+                        ${checkSocial(index)}
+                    </div>
+                    <p class="personalSite">${index.site}</p>
+                </div>
+            </div>
+        </div>
+        `
     });
     document.getElementById('teamList').innerHTML = html;
+}
+
+const checkSocial = (person) => {
+    let html = '';
+    if (person.socials[0].fb != '') {
+        html += `<a href="${person.socials[0].fb}" target="_blank" 
+        rel="noopener noreferrer"><i class="fab fa-facebook-square"></i></a>
+        `;
+    }
+    if (person.socials[1].ig != '') {
+    html += `<a href="${person.socials[1].ig}" target="_blank" 
+        rel="noopener noreferrer"><i class="fab fa-instagram-square"></i></a>
+        `;
+    }
+    if (person.socials[2].in != '') {
+    html += `<a href="${person.socials[2].in}" target="_blank" 
+        rel="noopener noreferrer"><i class="fab fa-linkedin"></i></a>
+        `;
+    }
+    console.log(person.socials[0].fb);
+    return html;
 }
