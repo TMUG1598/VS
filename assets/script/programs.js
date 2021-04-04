e710234aaec02df3784fb9503c009e17a838b544
Tatyana Gonzaga <tmugonzaga@gmail.com>
2021-04-04 12:13:57 +0800

    v5
    - new folders
    - final edits
    - updated SEO

---
+++ assets/script/programs.js
@@ -0,0 +1,32 @@
+var programs = [
+    {
+        'title':'Virtual Assistant Jumpstart',
+        'instructor':'Ysabelle David & Junnette Cerdeña',
+        'image':'assets/img/Programs/VABasics.png',
+        'page':'VAJumpstart.html',
+        'description': `
+        If you’re tired of working your regular 7-7 job and want to spend more time at home with your family, then a Virtual Assistant career path is for you!
+        <br><br>
+        We were in the same boat as you years ago - knowing you want to switch careers but not knowing where to start. That’s why we created this Virtual Assistant course!
+        <br><br>
+        A course designed by VAs for Filipinos aspiring to become VAs! Here, we lay out the basic things you need to do in order to Make the Switch from your 9-5 job to a home-based job!
+        `,
+    },
+    {
+        'title':'VA Accelerator',
+        'instructor':'Ysabelle David & Junnette Cerdeña',
+        'image':'assets/img/Programs/VASpecifics.png',
+        'page':'VAAccelerator.html',
+        'description': `
+        After learning what it means to become a Virtual Assistant, and learning the basics, we will go through everything else that you would need to officially become a well-earning Virtual Assistant. 
+        <br><br>
+From deciding what kind of VA you want to become to setting up the tools that will warrant your success, we have it all for you. 
+<br><br>
+By the end of this course, you will be locked, ready, and loaded to change your life. 
+<br><br>
+Bonus: A little extra for you guys to make sure that you won’t get lost on the first few moments of your VA life.
+<br><br>
+**Bonus Content will be unlocked 14 days after enrollment.`,
+    },
+
+];
